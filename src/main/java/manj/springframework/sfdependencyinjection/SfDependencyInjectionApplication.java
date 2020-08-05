5366ab3e9c8e8e55e86a379853c036873e55c5a6
sojivarkey <sojivarkey@sojis-air>
2020-08-04 19:04:04 -0500

    Initial Project Add

---
+++ src/main/java/manj/springframework/sfdependencyinjection/SfDependencyInjectionApplication.java
@@ -0,0 +1,13 @@
+package manj.springframework.sfdependencyinjection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfDependencyInjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfDependencyInjectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/manj/springframework/sfdependencyinjection/SfDependencyInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package manj.springframework.sfdependencyinjection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfDependencyInjectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
